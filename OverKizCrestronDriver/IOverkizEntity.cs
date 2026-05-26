08a0dfb2e0f48e40b1c3904c1fc87713f869da46
Neil Colvin <ncolvin@marvelous.com>
2026-05-26 12:06:48 +0100

    Switch license to MIT + Commons Clause (no sale)

--- OverKizCrestronDriver/IOverkizEntity.cs
+++ OverKizCrestronDriver/IOverkizEntity.cs
@@ -1,5 +1,5 @@
 // Copyright © 2026 Neil Colvin.
-// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// Licensed under the MIT License with Commons Clause. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
 
